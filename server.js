f42ae8d839f061bca17e6198fa33c04601d2bc84
wangjun <wangjunneil@gmail.com>
2026-04-21 21:59:35 +0800

    跨域问题

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const PROJECTS_DIR = path.join(DATA_DIR, "projects");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(ROOT, "views"));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "2mb" }));
 app.use(cookieParser("secret"));
